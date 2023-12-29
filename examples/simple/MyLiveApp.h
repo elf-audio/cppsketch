2700bee78ae6e7beb6e6fa762c812313edd98a10
Marek-mba <bereza@gmail.com>
2023-12-29 18:33:47 +0000

    formatting

--- examples/simple/MyLiveApp.h
+++ examples/simple/MyLiveApp.h
@@ -6,18 +6,15 @@
 
 class MyLiveApp : public LiveApp {
 public:
-
-	void setup() override {
-		printf("setup\n");
-	}
+	void setup() override { printf("setup\n"); }
 
 	int i = 0;
 
-	void update() override { 
-		i+=1;
+	void update() override {
+		i += 1;
 		printf("update %d\n", i);
 
 		// sleep for 0.1 seconds
-		usleep(1000*1000);
+		usleep(1000 * 1000);
 	}
 };
